cfacd860464758b392c1a297fae87d96f872a2e9
JoaoNogueira23 <joao.n.m23@gmail.com>
2024-12-07 12:53:02 -0300

    fix: type column tables

--- model/table.go
+++ model/table.go
@@ -1,7 +1,7 @@
 package model
 
 type Table struct {
-	ID           int    `json:"id_table"`
+	ID           string `json:"id_table"`
 	Table_number string `json:"table_number"`
 	IsOccupied   bool   `json:"isOccupied"`
 	Location     string `json:"location"`
