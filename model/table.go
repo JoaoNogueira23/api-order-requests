0a0502bbd35fcffeeeedebfc9cace10094a21ce6
JoaoNogueira23 <joao.n.m23@gmail.com>
2025-05-07 22:06:32 -0300

    fix: ajuste para coluna opcional

--- model/table.go
+++ model/table.go
@@ -3,6 +3,6 @@ package model
 type Table struct {
 	ID           string `json:"id_table"`
 	Table_number string `json:"table_number"`
-	IsOccupied   bool   `json:"isOccupied"`
+	IsOccupied   *bool  `json:"isOccupied"`
 	Location     string `json:"location"`
 }
